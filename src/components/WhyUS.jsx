4c631f1f356b3f0a7be3cd70f31d136013a5a653
Izimartin <izimartinz@gmail.com>
2023-05-15 08:59:42 +0100

    Responsive Whyus Section

--- src/components/WhyUS.jsx
+++ src/components/WhyUS.jsx
@@ -3,26 +3,25 @@ import Card from "./reusables/Card";
 
 function WhyUS() {
 	return (
-		<div className='bg-[#f7fdff] lg:px-20 px-20 py-20'>
+		<div className='bg-[#f7fdff] lg:px-20 px-4 py-8 lg:py-20'>
 			<div className='container flex flex-col items-center justify-center mx-auto'>
-				<div className='w-full lg:w-2/2 mx-auto'>
+				<div className='w-full mx-auto'>
 					<div className='flex flex-col justify-center max-w-3xl mx-auto'>
-						<h3 className='text-3xl font-bold sm:text-4xl text-center'>
+						<h3 className='text-2xl sm:text-3xl md:text-4xl font-bold text-center'>
 							Why us?
 						</h3>
-
-						<p className='mt-6 mb-8 text-center'>
+						<p className='mt-4 sm:mt-6 mb-8 sm:mb-text-center'>
 							Take control of your requests and tasks from wherever you are in
-							the world all displayed in real time for you. Confirm whatever
+							the world, all displayed in real time for you. Confirm whatever
 							tasks that have been assigned as completed so you don’t miss a
 							thing. Converse with your housekeeper to get human feedback on
-							activities or change going on at home
+							activities or changes going on at home.
 						</p>
 					</div>
 				</div>
 			</div>
 
-			<div className='flex flex-col sm:flex-row'>
+			<div className='flex flex-col sm:flex-row justify-center'>
 				<Card
 					backgroundColor='#69C9DE'
 					icon='/src/assets/Icons/Icon-1.png'
