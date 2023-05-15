34445fe035eb37ecb412bb005766d7b715f7fc3f
Izimartin <izimartinz@gmail.com>
2023-05-15 08:58:45 +0100

    Responsive Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,23 +3,24 @@ import Button from "./reusables/Button";
 
 function Hero() {
 	return (
-		<div className=' bg-white lg:px-20 px-20 py-0'>
+		<div className='bg-white lg:px-20 px-4 py-8 sm:py-0'>
 			<div className='container flex flex-col-reverse justify-center items-center mx-auto sm:flex-row'>
 				<div className='w-full lg:w-1/2'>
 					<div className='flex flex-col justify-center max-w-lg mx-auto lg:ml-0 lg:mr-auto lg:max-w-none'>
 						<h1
-							className='text-5xl font-bold sm:text-6xl'
-							style={{ lineHeight: "4.5rem" }}>
+							className='text-4xl sm:text-5xl md:text-6xl font-bold mb-6 sm:mb-8 text-center sm:text-left'
+							style={{ lineHeight: "1.2" }}>
 							Feel at ease <br className='block' />
 							<span>on all your trips</span>
 						</h1>
-
-						<p className='mt-6 mb-8 '>
+						<p className='mb-6 sm:mb-8'>
 							We look after your home and environs while you're{" "}
 							<br className='block' />
 							away, providing the relaxation you deserve.
 						</p>
-						<div className='flex flex-col space-y-4 sm:items-center sm:flex-row sm:space-y-0 sm:space-x-4'>
+					</div>
+					<div className='flex flex-col mt-4 space-y-4 items-center sm:flex-row sm:space-y-0 sm:space-x-4'>
+						<div className='sm:text-center'>
 							<Button text='Get Started Today' />
 						</div>
 					</div>
