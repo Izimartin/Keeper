bd80115935ee4b58f2935c22bbca07c58c4484c2
Izimartin <izimartinz@gmail.com>
2023-05-15 09:00:00 +0100

    Adding Card Componet

--- src/components/reusables/Card.jsx
+++ src/components/reusables/Card.jsx
@@ -5,14 +5,13 @@ import React from "react";
 const Card = ({ backgroundColor, icon, heading, isLast }) => {
 	const cardStyle = {
 		backgroundColor,
-		marginRight: isLast ? "0" : "1rem", // Adjust the spacing as needed
 	};
 
 	return (
-		<div className='sm:w-1/2 mb-10 mt-10'>
+		<div className='sm:w-1/2 mb-4 sm:mb-10 mt-10'>
 			<div
 				style={cardStyle}
-				className='rounded-lg px-20 py-16 flex items-center'>
+				className='rounded-lg px-20 py-16 flex items-center sm:mr-4'>
 				<img src={icon} alt='Card Icon' className='w-20 h-20 mr-4' />
 				<h4 className='text-2xl text-white font-bold mr-10'>{heading}</h4>
 			</div>
