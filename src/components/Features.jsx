5810c7bbc0b68c0050dda2857a24c95d1003e7d7
Izimartin <izimartinz@gmail.com>
2023-05-15 08:58:21 +0100

    Responsive Features Section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Hero() {
 	return (
-		<div className=' bg-[#ffffef] lg:px-20 px-20 py-6'>
-			<div className=' container flex flex-col-reverse justify-center items-center mx-auto sm:flex-row'>
-				<div className='w-full lg:w-1/2 mr-20'>
+		<div className='bg-[#ffffef] lg:px-20 px-4 py-6'>
+			<div className='container flex flex-col-reverse justify-center items-center mx-auto sm:flex-row'>
+				<div className='w-full lg:w-1/2 mr-0 sm:mr-20'>
 					<img
 						src='/src/assets/img/features-img.png'
 						alt='Hero Image'
@@ -14,17 +14,17 @@ function Hero() {
 				<div className='w-full lg:w-1/2'>
 					<div className='flex flex-col justify-center max-w-lg mx-auto lg:ml-0 lg:mr-auto lg:max-w-none'>
 						<h4
-							className='text-3xl font-bold sm:text-4xl'
-							style={{ lineHeight: "3rem" }}>
+							className='text-2xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-6 text-center sm:text-left'
+							style={{ lineHeight: "1.5", marginTop: "2rem" }}>
 							Let us do the worrying for <br className='block' />
 							<span className='text-[#c959b7]'>you</span>
 						</h4>
 
-						<p className='mt-6 mb-8 '>
+						<p className='mt-4 mb-6 sm:mb-8'>
 							Fly anywhere in the world without a hitch of worry, we’ll cover
 							cleaning, decoration, pet care and so much more! Our dedicated
 							team makes it their core goal to keep your home safe and void of
-							issues or discrepancies
+							issues or discrepancies.
 						</p>
 					</div>
 				</div>
